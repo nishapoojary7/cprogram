a319e6e3c7dc49668dc1bd01ffc661010f6c3e92
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-02-25 23:07:12 +0530

    Add files via upload

---
+++ fizzbuzz.c
@@ -0,0 +1,27 @@
+ 
+#include <stdio.h>
+ 
+int main(void)
+{
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
