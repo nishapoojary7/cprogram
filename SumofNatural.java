a6c81f047c77774a40e801948e734e44c9a6a4d5
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-05-05 23:15:32 +0530

    Add files via upload

---
+++ SumofNatural.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class SumofNatural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,num=10,sum=0;
+		for(i=1;i<=num;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("sum of natural numbers"+sum);
+
+	}
+
+}
