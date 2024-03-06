860a71d3c947a532835072f9d621e4efca75486a
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2024-03-06 20:04:32 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+     printf("positive");
+   }
+}
